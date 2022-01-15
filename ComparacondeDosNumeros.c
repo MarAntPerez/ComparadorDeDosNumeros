345263e8aa674ef444450b46d96c648e39cc6e46
Marco <maphy1707@gmail.com>
2022-01-14 18:05:25 -0600

    Compara dos numeros if

---
+++ ComparacondeDosNumeros.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int numeroOne;
+	int numeroTwo;
+	
+	printf("Ingresa un numero: ");
+	scanf("%i", &numeroOne);
+	printf("Ingresa otro numero: ");
+	scanf("%i", &numeroTwo);
+	
+	if(numeroOne > numeroTwo){
+		printf("El numero %i es mayor al numero %i", numeroOne, numeroTwo);
+	}else if(numeroTwo > numeroOne){
+		printf("El numero %i es mayor al nummero %i", numeroTwo, numeroOne);
+	}else{
+		printf("Los numeros son iguales.");
+	}
+	
+	return 0;
+}
